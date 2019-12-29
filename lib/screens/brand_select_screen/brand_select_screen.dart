07dcf15b89500c1df560af73a2b109ddb6d8fb23
Ali-Amin <ali.m.amin98@gmail.com>
2019-12-29 13:15:55 +0200

    Brand selection page

---
+++ lib/screens/brand_select_screen/brand_select_screen.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:grad/blocs/bloc.dart';
+import 'package:grad/models/brand.dart';
+import 'package:grad/screens/problem/problem.dart';
+import 'package:provider/provider.dart';
+
+class BrandSelectScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final Bloc bloc = Provider.of(context);
+    return Scaffold(
+      body: Center(
+        child: StreamBuilder<List<Brand>>(
+            stream: bloc.brands$,
+            builder: (_, snapshot) {
+              if (!snapshot.hasData) {
+                return CircularProgressIndicator();
+              }
+              final List<Brand> brands = snapshot.data;
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text("Choose Brand"),
+                  SizedBox(
+                    height: 200,
+                    child: ListView(
+                      physics: BouncingScrollPhysics(),
+                      shrinkWrap: true,
+                      scrollDirection: Axis.horizontal,
+                      children: brands
+                          .map(
+                            (brand) => BrandCard(
+                              brand: brand,
+                              onPressed: () => Navigator.of(context).push(
+                                MaterialPageRoute(
+                                  builder: (_) => ProblemScreen(),
+                                ),
+                              ),
+                            ),
+                          )
+                          .toList(),
+                    ),
+                  ),
+                ],
+              );
+            }),
+      ),
+    );
+  }
+}
+
+class BrandCard extends StatelessWidget {
+  final Brand brand;
+  final Function() onPressed;
+  const BrandCard({Key key, @required this.brand, this.onPressed})
+      : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onPressed,
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        width: 200,
+        alignment: Alignment.center,
+        child: Image(
+          image: NetworkImage(brand.photoUrl),
+        ),
+      ),
+    );
+  }
+}
