ca350cc2477525a24b88b926269b8d227b9df206
Ali-Amin <ali.m.amin98@gmail.com>
2019-12-29 13:16:34 +0200

    onPress logic

--- lib/screens/problem/problem.dart
+++ lib/screens/problem/problem.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:grad/screens/car_select_screen/car_select_screen.dart';
 
 class ProblemScreen extends StatelessWidget {
   @override
@@ -9,13 +10,23 @@ class ProblemScreen extends StatelessWidget {
           physics: BouncingScrollPhysics(),
           shrinkWrap: true,
           children: <Widget>[
-            Container(
-              color: Colors.blue,
-              margin: const EdgeInsets.all(8),
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                "Breaks",
-                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+            InkWell(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (_) => CarSelectScreen(),
+                  ),
+                );
+              },
+              child: Container(
+                color: Colors.blue,
+                margin: const EdgeInsets.all(8),
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  "Breaks",
+                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                ),
               ),
             ),
             Container(
